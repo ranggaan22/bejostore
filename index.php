e531c988709dbee93aacc18f62af99587d0b49ba
ranggaan22 <95576964+ranggaan22@users.noreply.github.com>
2021-12-08 13:07:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = 'SB-Mid-server-KO6-xcqJuHUUhMet-laSzrKN';
+$server_key = "SB-Mid-server-KO6-xcqJuHUUhMet-laSzrKN";
 // Set true for production, set false for sandbox
 $is_production = false;
 
